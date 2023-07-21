185e62f1344983bec0257c64107c64458e27aea1
Sebastián Romero <romsebastian@gmail.com>
2023-07-21 19:12:01 -0400

    Generic React default function & seb.js

---
+++ generic_react.js
@@ -0,0 +1,9 @@
+function Mytag(){
+			return ( 
+
+			)
+		}
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<Mytag />)
+
+OneDrive/Lambton college/1stTerm/Frontend/project
---
+++ seb.js
@@ -0,0 +1,7 @@
+function Mytag(){
+			return ( 
+
+			)
+		}
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<Mytag />)
